befcfa8aefc66e187a54207c884f645ace57d94b
QuinEddy <a.reinini@gmail.com>
2026-08-19 15:39:06 +0200

    Primo tentativo con memoria NON persistente

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from "express";
 
+// Definiamo l'interfaccia per la sessione
+interface Session {
+  id: number;
+  name: string;
+  status: string;
+}
+
+//Memoria temporanea per le sessioni. NON PERSISTENTE.
+const sessions: Session[] = [];
+
 const app = express();
 
 const PORT = 3000;
 
-// Middleware to parse JSON request bodies
+// Middleware JSON per il parsing del corpo delle richieste
 app.use(express.json());
 
 app.get("/hello", (req, res) => {
@@ -13,13 +23,19 @@ app.get("/hello", (req, res) => {
   });
 });
 
+app.get("/session", (req, res) => {
+  res.json(sessions);
+});
+
 app.post("/session", (req, res) => {
   const { name } = req.body;
-  res.status(201).json({
-    id: 1,
+  const newSession: Session = {
+    id: sessions.length + 1,
     name: name,
     status: "ACTIVE"
-  });
+  };
+  sessions.push(newSession);
+  res.status(201).json(newSession);
 });
 
 app.listen(PORT, () => {
