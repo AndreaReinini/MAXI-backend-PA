26f9025ceee2af523b682d715609c3cbef7688dd
QuinEddy <a.reinini@gmail.com>
2026-08-20 13:07:47 +0200

    Sessioni in memoria persistente CRUD sequelize

--- src/controllers/sessionController.ts
+++ src/controllers/sessionController.ts
@@ -1,59 +1,47 @@
 import type { Request, Response } from "express";
-
-// TEMPORANEO - Memoria temporanea per le sessioni. NON PERSISTENTE.
-
-// Definiamo l'interfaccia per la sessione
-interface Session {
-  id: number;
-  name: string;
-  status: string;
-}
-
-//Memoria temporanea per le sessioni. NON PERSISTENTE.
-const sessions: Session[] = [];
-let nextSessionId = 1;
+import SessionModel from "../models/Session.js";
 
 // Funzione per ottenere tutte le sessioni
-export function getSessions(req: Request, res: Response) {
+export async function getSessions(req: Request, res: Response) {
+    const sessions = await SessionModel.findAll();
     res.json(sessions);
 }
 
-export function getSessionById(req: Request, res: Response) {
+export async function getSessionById(req: Request, res: Response) {
     const id = Number(req.params.id);
-    const session = sessions.find((session) => session.id === id);
+    const session = await SessionModel.findByPk(id);
     if (!session) {
         return res.status(404).json({ message: "Session not found" });
     }
     res.json(session);
 }
 
-export function updateSession(req: Request, res: Response) {
+export async function updateSession(req: Request, res: Response) {
   const id = Number(req.params.id);
-  const session = sessions.find((session) => session.id === id);
+  const session = await SessionModel.findByPk(id);
   if (!session) {
     return res.status(404).json({ message: "Session not found" });
   }
-  session.name = req.body.name.trim();
+  await session.update({ name: req.body.name.trim() });
   res.json(session);
 }
 
-export function createSession(req: Request, res: Response) {
+export async function createSession(req: Request, res: Response) {
     const { name } = req.body;
-    const newSession: Session = {
-        id: nextSessionId++,
-        name: name,
-        status: "ACTIVE"
-    };
-    sessions.push(newSession);
+
+    const newSession = await SessionModel.create({
+      name
+    });
+
     res.status(201).json(newSession);
 }
 
-export function deleteSession(req: Request, res: Response) {
+export async function deleteSession(req: Request, res: Response) {
   const id = Number(req.params.id);
-  const sessionIndex = sessions.findIndex((session) => session.id === id);
-  if (sessionIndex === -1) {
+  const session = await SessionModel.findByPk(id);
+  if (!session) {
     return res.status(404).json({ message: "Session not found" });
   }
-  sessions.splice(sessionIndex, 1);
+  await session.destroy();
   res.status(204).send();
 }
